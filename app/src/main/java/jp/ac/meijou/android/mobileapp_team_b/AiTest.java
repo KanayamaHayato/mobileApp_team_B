c6f8e579c78a627a2367dee501fe4c4489d7d070
Takuma Oguri <183122712+ogu618@user.noreplay.github.com>
2025-10-20 15:11:58 +0900

    EfficientNet-Liteを使うための準備 画像を分類することができるようになった 画像のサイズが大きいとクラッシュするため注意

--- app/src/main/java/jp/ac/meijou/android/mobileapp_team_b/AiTest.java
+++ app/src/main/java/jp/ac/meijou/android/mobileapp_team_b/AiTest.java
@@ -1,5 +1,7 @@
 package jp.ac.meijou.android.mobileapp_team_b;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 
@@ -9,9 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.label.Category;
+import org.tensorflow.lite.task.vision.classifier.Classifications;
 import org.tensorflow.lite.task.vision.classifier.ImageClassifier;
 
 import java.io.IOException;
+import java.util.List;
 
 import jp.ac.meijou.android.mobileapp_team_b.databinding.ActivityAiTestBinding;
 import jp.ac.meijou.android.mobileapp_team_b.databinding.ActivityMainBinding;
@@ -52,6 +58,31 @@ public class AiTest extends AppCompatActivity {
     }
 
     private void classifyImage() {
+        // Drawable から画像を取得
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample);
 
+        // Bitmap を TensorImage に変換
+        TensorImage image = TensorImage.fromBitmap(bitmap);
+
+        // 推論実行
+        List<Classifications> results = imageClassifier.classify(image);
+
+        // 結果を画面に表示
+        if (results != null && results.size() > 0) {
+            // 最初の分類セット（通常は1つ）
+            List<Category> categories = results.get(0).getCategories();
+            if (categories != null && categories.size() > 0) {
+                Category topResult = categories.get(0);
+                String label = topResult.getLabel();
+                float score = topResult.getScore();
+                String resultText = "予測: " + label + "\n信頼度: " + (score * 100) + " %";
+                binding.AISampleResultText.setText(resultText);
+            } else {
+                binding.AISampleResultText.setText("分類結果なし");
+            }
+        } else {
+            binding.AISampleResultText.setText("予測できませんでした");
+        }
     }
+
 }
