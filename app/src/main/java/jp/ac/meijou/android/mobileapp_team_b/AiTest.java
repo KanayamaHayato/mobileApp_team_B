62eeec0570888070012bfb7a4440916dffe268bd
Takuma Oguri <183122712+ogu618@user.noreplay.github.com>
2025-10-20 14:50:00 +0900

    EfficientNet-Liteを使うための準備 bindingの設定など最初の準備 現段階ではTFLite Task Libraryとの依存関係がないためエラーになっていると思われる

--- app/src/main/java/jp/ac/meijou/android/mobileapp_team_b/AiTest.java
+++ app/src/main/java/jp/ac/meijou/android/mobileapp_team_b/AiTest.java
@@ -1,5 +1,6 @@
 package jp.ac.meijou.android.mobileapp_team_b;
 
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +9,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
+
+import jp.ac.meijou.android.mobileapp_team_b.databinding.ActivityAiTestBinding;
+import jp.ac.meijou.android.mobileapp_team_b.databinding.ActivityMainBinding;
+
 public class AiTest extends AppCompatActivity {
+
+    private ActivityAiTestBinding binding;
+    private ImageClassifier imageClassifier;
+    private ConnectivityManager connectivityManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_ai_test);
+
+        binding = ActivityAiTestBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        // EfficientNet モデルをロード
+        try {
+            imageClassifier = ImageClassifier.createFromFile(this, "efficientnet-lite0-int8.tflite");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // ボタンのクリック処理
+        binding.AISampleButton.setOnClickListener(view -> {
+            classifyImage();
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
